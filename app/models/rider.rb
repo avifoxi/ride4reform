45b2852497e000a5aaa6bf51f0f41fe3fe4c7312
AviFoxi <frosenox@gmail.com>
2014-10-03 13:45:06 -0400

    add address model

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -2,6 +2,9 @@ class Rider < ActiveRecord::Base
   has_many :donations
   has_many :donors, through: :donations
 
+  has_one :address, :as => :addressable
+  accepts_nested_attributes_for :address
+
   RIDE_OPTIONS = ['Original Track', 'Light Track', 'Hiking', 'Combination Hiking/Riding']
 
   TITLES = ['Mr', 'Ms', 'Dr', 'Rabbi', 'Cantor', 'Chazzan', 'Educator', 'None']
