dfb3ad58a1fcda6e059dbfbcc5142f2909417635
AviFoxi <frosenox@gmail.com>
2014-10-03 13:19:52 -0400

    add ride_options constant to rider model, with class method for display

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,4 +1,12 @@
 class Rider < ActiveRecord::Base
   has_many :donations
   has_many :donors, through: :donations
+
+  RIDE_OPTIONS = ['Original Track', 'Light Track', 'Hiking', 'Combination Hiking/Riding']
+
+  def self.ride_options 
+  	RIDE_OPTIONS
+  end
+
+
 end
