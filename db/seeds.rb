30053555e0b2e1d0e864551f881159281131e0a9
AviFoxi <frosenox@gmail.com>
2014-10-05 17:31:11 -0400

    finalize initial seed, correct new.html for @user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 case Rails.env
 when "development"
+
+
 
 	15.times do 
 		User.create(title: User.titles.sample, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password )
@@ -27,6 +22,7 @@ when "development"
 	20.times do 
 		Donation.create(amount: amounts.sample, donor: users.sample, rider: RiderReg.all.sample.rider)
 	end
+
 
 when "production"
    
