159177a6e6a807b7cc47e284cde8fa01f4557b86
Alan Cohen <awc583@gmail.com>
2014-10-06 15:24:34 -0400

    initial setup of donations controller to test hooks to paypal api

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,7 +1,57 @@
 class DonationsController < ApplicationController
   include PayPal::SDK::REST
 
+  def index
+    ##DEBUG ROUTE TO GET ALL TRANSACTIONS
+    p Payment.all.payments.first.id
+  end
+
   def new
-    @donation = Payment.new
+    ##DEBUG CODE TO MAKE IT EASIER TO HIT A ROUTE TO TEST TRANSACTION
+    PayPal::SDK::REST.set_config(
+      :mode => "sandbox", # "sandbox" or "live"
+      :client_id => ENV['PAYPAL_CLIENT_ID'],
+      :client_secret => ENV['PAYPAL_SECRET'])
+
+    # Build Payment object
+    @payment = Payment.new({
+      :intent => "sale",
+      :payer => {
+        :payment_method => "credit_card",
+        :funding_instruments => [{
+          :credit_card => {
+            :type => "visa",
+            :number => "4417119669820331",
+            :expire_month => "11",
+            :expire_year => "2018",
+            :cvv2 => "874",
+            :first_name => "Joe",
+            :last_name => "Shopper",
+            :billing_address => {
+              :line1 => "52 N Main ST",
+              :city => "Johnstown",
+              :state => "OH",
+              :postal_code => "43210",
+              :country_code => "US" }}}]},
+      :transactions => [{
+        :item_list => {
+          :items => [{
+            :name => "item",
+            :sku => "item",
+            :price => "1",
+            :currency => "USD",
+            :quantity => 1 }]},
+        :amount => {
+          :total => "1.00",
+          :currency => "USD" },
+        :description => "This is the payment transaction description." }]})
+
+    # Create Payment and return the status(true or false)
+    if @payment.create
+      p @payment.id     # Payment Id
+    else
+      p @payment.error  # Error Hash
+    end
+
   end
 end
