0e7fc9bb37b6a854ab06183f660ac6e0f4d24da3
AviFoxi <frosenox@gmail.com>
2014-10-08 17:11:47 -0400

    add full name method to rr model

--- app/models/rider_reg.rb
+++ app/models/rider_reg.rb
@@ -16,5 +16,9 @@ class RiderReg < ActiveRecord::Base
   	perc = (self.raised.to_f / self.goal.to_f).round(2) * 100
   	perc.to_i.to_s + '%'
   end
+
+  def full_name
+    self.title + ' ' + self.first_name + ' ' + self.last_name
+  end
   
 end
