53cf6de5f5f335bf3b38212a510355f49ed160d8
AviFoxi <frosenox@gmail.com>
2014-10-08 15:41:30 -0400

    add goal and raised to seed file data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,15 @@ when "development"
 	end
 
 	users = User.all.to_a
+	amounts = (500..5000).to_a
 	   
 
 	5.times do 
 		user = users.pop
-		RiderReg.create(rider: user, ride_option: RiderReg.ride_options.sample, birthdate: Date.new(1982,4,19) )
+		RiderReg.create(rider: user, ride_option: RiderReg.ride_options.sample, birthdate: Date.new(1982,4,19), goal: amounts.sample, raised: amounts.sample )
 	end
 
 	donors = (0..9).to_a
-	amounts = (500..5000).to_a
 	tf = [true, false]
 
 	20.times do 
