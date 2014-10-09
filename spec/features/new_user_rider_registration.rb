cf77a7ac51887c55c6b5675b17b24aa7a1b02050
AviFoxi <frosenox@gmail.com>
2014-10-09 12:38:12 -0700

    capybar dsl not recognized -- why?

--- spec/features/new_user_rider_registration.rb
+++ spec/features/new_user_rider_registration.rb
@@ -1,12 +1,14 @@
-require 'rails_helper'
+
 
 describe "the signin process", :type => :feature do
+
+
   # before :each do
   #   User.make(:email => 'user@example.com', :password => 'caplin')
   # end
 
   it "allows new user to create basic account" do
-    visit '/'
+    visit '/rider_regs'
     click_button 'Register Now!'
     expect(page).to have_content 'Success'
   end
