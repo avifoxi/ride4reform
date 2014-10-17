fdabd8b49233683a25de60c4bbdb7ba459f10b9d
AviFoxi <frosenox@gmail.com>
2014-10-17 18:46:18 -0400

    fleshing out donation feature test

--- spec/features/feature_helper_methods.rb
+++ spec/features/feature_helper_methods.rb
@@ -1,32 +1,31 @@
-def register_new_rider
-  describe "automation" do
-	  visit '/'
-	  click_link 'Register Now!'
-	  expect(page).to have_content 'Sign Up'
+def register_new_rider_logout
+  visit '/'
+  click_link 'Register Now!'
+  expect(page).to have_content 'Sign Up'
 
-	  select('Mr', :from => 'Title')
-	  fill_in('First name', :with => 'Rider')
-	  fill_in('Last name', :with=> 'Test')
-	  fill_in('Email', :with=> 'alan@test.com')
-	  fill_in('Password', :with => 'password')
-	  fill_in('Password confirmation', :with=> 'password')
-	  click_button 'Sign up'
+  select('Mr', :from => 'Title')
+  fill_in('First name', :with => 'Rider')
+  fill_in('Last name', :with=> 'Test')
+  fill_in('Email', :with=> 'alan@test.com')
+  fill_in('Password', :with => 'password')
+  fill_in('Password confirmation', :with=> 'password')
+  click_button 'Sign up'
 
-	  expect(page).to have_content 'Please fill out your Rider Registration!'
+  expect(page).to have_content 'Please fill out your Rider Registration!'
 
-	  select('Original Track', :from=> 'Ride option')
-	  fill_in('Birthdate', :with=> '05071983')
-	  fill_in('Primary phone', :with=> '123-456-7890')
-	  fill_in('Secondary phone', :with=> '098-765-4321')
-	  fill_in('Goal', :with=> '123456')
-	  fill_in('Bio', :with=> 'This is my bio.')
-	  click_button 'Create Rider reg'
+  select('Original Track', :from=> 'Ride option')
+  fill_in('Birthdate', :with=> '05071983')
+  fill_in('Primary phone', :with=> '123-456-7890')
+  fill_in('Secondary phone', :with=> '098-765-4321')
+  fill_in('Goal', :with=> '123456')
+  fill_in('Bio', :with=> 'This is my bio.')
+  click_button 'Create Rider reg'
 
-	  expect(page).to have_content 'Read and Review These Terms of Entry'
+  expect(page).to have_content 'Read and Review These Terms of Entry'
 
-	  choose('accept_terms_true')
-	  click_button 'Update Rider reg'
+  choose('accept_terms_true')
+  click_button 'Update Rider reg'
 
-	  expect(page).to have_content 'Alan Cohen'
-	end  
+  expect(page).to have_content 'Rider Test' 
+  click_link 'Logout' 
 end
--- spec/features/new_user_donates_to_rider.rb
+++ spec/features/new_user_donates_to_rider.rb
@@ -3,9 +3,31 @@ require_relative 'feature_helper_methods'
 
 describe "the donation process" do
 	## call helper method to sign up and register a rider. now, just call lower method
-    it "user donates to registered rider" do
-        register_new_rider
+    it "unregisterd user donates to registered rider" do
+        register_new_rider_logout
+        click_link 'Donate Now!'
+        expect(page).to have_content 'Rider Test'
+        click_link 'Donate Now!'
+        select('Visa', :from=> 'Credit Card Type')
+        fill_in('Credit Card Number', :with => '4417119669820331')
+        fill_in('Month (MM)', :with => '11')
+        fill_in('Year (YYYY)', :with => '2018')
+        fill_in('CVV2', :with => '874')
+        fill_in('First Name', :with => 'Unregistered')
+        fill_in('Last Name', :with => 'Donor')
+        fill_in('Email', :with => 'donor@donor.com')
+        fill_in('Address', :with => '123 Donor Blvd')
+        fill_in('City', :with => 'Donor')
+        fill_in('State', :with => 'New York')
+        fill_in('Postal Code', :with => '11225')
+        fill_in('Donation', :with => '1000')
+        click_button 'Donate'
     end
 end
 
 
+# visa
+# 4417119669820331
+# 11
+# 2018
+# 874
