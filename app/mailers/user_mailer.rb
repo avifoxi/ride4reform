5bcf780f495fc5a57e17d3f592470624d0280506
Alan Cohen <awc583@gmail.com>
2014-10-23 09:03:10 -0400

    initial setup of donation receipt email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def donation_receipt(donation)
+    p @donor = donation.user.full_name
+    p @amount = donation.receipt.amount
+    p @email = donation.user.email
+    p @rider = donation.rider.full_name
+
+    mail(to: @email, subject: "Ride4Reform Donation Receipt")
+  end
 end
