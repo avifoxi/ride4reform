57123b0f095d0d79bc883cec50adba3c0410d6ae
AviFoxi <frosenox@gmail.com>
2014-10-28 17:01:54 -0400

    add attached file helper method to rider_reg model, and validation

--- app/models/rider_reg.rb
+++ app/models/rider_reg.rb
@@ -6,6 +6,21 @@ class RiderReg < ActiveRecord::Base
 	has_many :donations_received, through: :rider
   has_one :mailing_address, :as => :addressable
 
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :photo, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+
+
+
+
+
+
   RIDE_OPTIONS = ['Original Track', 'Light Track', 'Hiking', 'Combination Hiking/Riding']
 
   def self.ride_options 
