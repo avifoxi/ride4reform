c4e2d38528eabe861bd7a4770c3c3ade3b92ba5e
AviFoxi <frosenox@gmail.com>
2014-10-30 11:28:36 -0400

    update feature_helper_methods

--- spec/features/feature_helper_methods.rb
+++ spec/features/feature_helper_methods.rb
@@ -6,7 +6,7 @@ def register_new_rider_logout
   select('Mr', :from => 'Title')
   fill_in('First name', :with => 'Rider')
   fill_in('Last name', :with=> 'Test')
-  fill_in('Email', :with=> 'alan@test.com')
+  fill_in('Email', :with=> 'rider@test.com')
   fill_in('Password', :with => 'password')
   fill_in('Password confirmation', :with=> 'password')
   click_button 'Sign up'
@@ -26,6 +26,22 @@ def register_new_rider_logout
   choose('accept_terms_true')
   click_button 'Update Rider reg'
 
-  expect(page).to have_content 'Rider Test' 
+  expect(page).to have_content 'Registration Fee'
+  select('Visa', :from => 'Credit Card Type:')
+  fill_in('Credit Card Number:', :with=> '05071983')
+  fill_in('Month (MM):', :with=> '05')
+  fill_in('Year (YYYY):', :with=> '2017')
+  fill_in('CVV2:', :with=> '123')
+  fill_in('First Name:', :with=> 'Rider')
+  fill_in('Last Name:', :with=> 'Test')
+  fill_in('Address 1:', :with=> '1 Rider Row')
+  fill_in('City:', :with=> 'Riderstown')
+  fill_in('State:', :with=> 'CA')
+  fill_in('Postal Code:', :with=> '90035')
+
+  click_button 'Register'
+
+  expect(page).to have_content 'Rider Test'
+  expect(page).to have_content 'Edit Your Profile'
   click_link 'Logout' 
 end
