32535637c6cc0e29db7f62934c0a4e27501e81f1
Armen Vartan <vartan.armen@gmail.com>
2014-12-13 16:33:58 -0500

    validations for mailing_addresses model

--- app/models/mailing_address.rb
+++ app/models/mailing_address.rb
@@ -1,17 +1,20 @@
 class MailingAddress < ActiveRecord::Base
 	belongs_to :addressable, :polymorphic => true
 
+	validates :line1, :city, :state, :zip, presence: true
+	validates :city, format: { with: /[a-zA-Z]{3,}/, message: "Please enter a valid City." }
+	validates :zip, format: { with: /\d{5}/, message: "Please enter a valid zip code" }
+
 	def self.valid_types
 		["RiderReg", "Receipt"]
 	end
 
 	def one_liner
-		o_l = self.line1 + ' ' 
-		if self.line2 
+		o_l = self.line1 + ' '
+		if self.line2
 			o_l += (self.line2 + ' ')
 		end
 		o_l + self.city + ' ' + self.zip
 	end
 
 end
-
