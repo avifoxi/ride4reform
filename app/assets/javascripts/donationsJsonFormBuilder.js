4531b2b62809e84f59aadbd111398119b414d9d3
AviFoxi <frosenox@gmail.com>
2014-12-16 16:48:58 -0500

    form building

--- app/assets/javascripts/donationsJsonFormBuilder.js
+++ app/assets/javascripts/donationsJsonFormBuilder.js
@@ -5,31 +5,63 @@ function DonationsFormBuilder (els) {
 	this.queryPath = els['query_path'];
 	this.submitPath = els['submission_path'];
 	this.donationData = {};
-	this.complete = false;
+	// this.complete = false;
+	this.currentStep = 0;
+	this.donorExistsInDB = false;
 	this.$showProcess = $('.show_donation_in_process ul');
 	
 	var _this = this;
 
 	this.$form.submit(function(e){
 		e.preventDefault(e);
-		console.log(e);
-		_this.queryNBuild();
-		
+		_this.stepThrough();	
 	});
 
+	this.steps = {
+		0 : 'prepEmailAmount',
+		1 : 'getEmailAmount',
+		2 : 'prepAddress',
+		3 : 'getAddress'
+		// 3 : 'cc_form',
+		// 3 : 'privacyAndMessage',
+		// 4 : 'submit'
+	}
+
+	this.stepThrough();
 }
 
 DonationsFormBuilder.prototype = {
-	queryNBuild : function(){
-		var _this = this;
-		this.donationData.email = $('#email').val();
-
-		this.donationData.amount = $('#amount').val();
-		// 
-		// console.log(_this.donationData);
-		this.queryEmailMailingAddy(_this.donationData.email);
+	// init : function(){
+	// 	$('.donationSteps').hide();
+		
+	// },
+	stepThrough : function(){
+		var step = this.currentStep;
+		var stepFunc = this.steps[step];
+		this[stepFunc]();
 	},
-	queryEmailMailingAddy : function(email){
+
+	prepEmailAmount : function(){
+		$('.donationSteps').hide();
+		$('#emailAmount').show();
+		this.currentStep += 1;
+	},
+	getEmailAmount : function(){
+		console.log('firing');
+		var _this = this;
+
+		// TODO add validations...
+		var em = $('#email').val();
+		var am = $('#amount').val();
+		if ( em !== '' && am !== ''){
+			_this.donationData.email = em;
+			_this.donationData.amount = am;
+			_this.currentStep += 1;
+			_this.queryEmailInDBGetAddy(_this.donationData.email);	
+		}
+
+	},
+	queryEmailInDBGetAddy : function(email){
 		var _this = this;
 
 		$.ajax({
@@ -39,20 +71,22 @@ DonationsFormBuilder.prototype = {
 		  	email: email
 		  }
 		}).done(function(response) {
+		  console.log(response);
 		  if (response.donor){
 		  	// update screen with info
-		  	_this.updateShowProcess();
 		  	// give option to use existing mailing_addy
-		  	_this.referenceOldOrNewAddy(response.mailing_address);
+		  	_this.dbAddyOptionShow(response.mailing_address);
 		  } else {
 		  	// solicit User info for 
 		  }
+		  // _this.privacyAndMessage();
 		});	
 	},
 	updateShowProcess : function(){
 		var _this = this;
 		var data = _this.donationData;
 		var donationFields = Object.getOwnPropertyNames(_this.donationData);
+		// first clear before appending... 
 		for (var i=0; i < donationFields.length; i++ ){
 			var li = document.createElement('li');
 			li.innerHTML = donationFields[i] + ': ' +
@@ -60,9 +94,9 @@ DonationsFormBuilder.prototype = {
 			this.$showProcess.append(li);
 		}
 	},
-	referenceOldOrNewAddy : function(addy){
-		$('.addyInsert').text(addy) ;
-		
+	dbAddyOptionShow : function(addy){
+		// $('#emailAmount').hide();
+		$('.addyInsert').text(addy);
 	}
 }
 
